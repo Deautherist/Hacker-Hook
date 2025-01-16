8cab71c6e2e72bf6c004525eecb98c949cce557e
Deautherist <69185581+Deautherist@users.noreply.github.com>
2025-01-16 16:44:45 +0800

    Update ngrok-installer.sh
    We added a short script to run and install ngrok automatically. If this program doesn't help you. you can manually download ngrok from their official site here: https://download.ngrok.com/linux

--- ngrok-installer.sh
+++ ngrok-installer.sh
@@ -15,6 +15,14 @@ else
             unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
             chmod +x ngrok
             rm -rf ngrok-stable-linux-arm.zip
+        elif
+            curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+              | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+              && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+              | sudo tee /etc/apt/sources.list.d/ngrok.list \
+              && sudo apt update \
+              && sudo apt install ngrok
+
         else
             printf "${BRed}Error System cannot detect, ${BGreen}install NGROK manually\n"
             printf "${BGreen}from this site: https://download.ngrok.com/linux\n"
